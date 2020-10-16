ad7568472c694e732c0251a3291d7e42107b6d65
Zalak <zalakpatel534@gmail.com>
2020-10-16 15:37:57 +0530

    create csv files for all wednesdays in a year and then append all days before that csv file

--- main.py
+++ main.py
@@ -6,18 +6,19 @@ from datetime import date
 
 
 # create empty list for months and then append month name in it
-directory_names = []
+dir_names = []
 for month_num in range(1, 13):
-    directory_names.append(calendar.month_name[month_num])
+    dir_names.append(calendar.month_name[month_num])
 print("successfully got all months ")
 
 dir_path = os.getcwd()
 
-# Returns all Wednesdays of a given year
+# return all Wednesdays of a given year
 year = 2020
 obj = calendar.TextCalendar(calendar.SUNDAY)
 wednesdays = []
-
+days = []
+rows_header = ['year', 'month', 'date']
 
 try:
     # get wednesdays
@@ -25,24 +26,30 @@ try:
         for i in obj.itermonthdays(year, month):
             if i != 0:
                 day = date(year, month, i)
+                days.append(day)
                 if day.weekday() == 2:                  # if its WEDNESDAY
                     wednesdays.append(day)
 
 # create directories
-    for directory_name in directory_names:
-        path = os.path.join(dir_path, directory_name)
+    for dir_name in dir_names:
+        path = os.path.join(dir_path, dir_name)
         os.mkdir(path)
-        print("directory '{}' created".format(directory_name))
-        for wednesday in wednesdays:
-            file_name = wednesday.strftime('%B')        # get wednesday's month
+        print("directory '{}' created".format(dir_name))
 
-            # create csv files
-            if directory_name == file_name:             # if directory name (month) is matching to wednesday's month
-                with open(os.path.join(dir_path, directory_name, str(wednesday) + '.csv'), 'w') as csvfile:
-                    # creating a csv writer object
-                    csvwriter = csv.writer(csvfile)
-            else:
-                print("doesn't match")
+        for wednesday in wednesdays:
+            for day in days:
+                month = wednesday.strftime('%B')        # get wednesday's month
+
+                # create csv files
+                if dir_name == month:             # if directory name (month) is matching to wednesday's month
+                    with open(os.path.join(dir_path, dir_name, str(wednesday) + '.csv'), 'a') as csvfile:
+                        # creating a csv writer object
+                        csvwriter = csv.writer(csvfile)
+                        if wednesday > day:
+                            day = str(day).split('-')
+                            day = [day[0], day[1], day[2]]
+                            csvwriter.writerow(day)
+                            print("created csv file '{}'".format(wednesday))
 
 
 except Exception as error:
